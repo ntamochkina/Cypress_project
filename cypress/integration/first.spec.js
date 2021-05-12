a6db6db7ad28cc88a699967315d298ea47c34912
Dunice <dunice@Dunices-Mac-mini-5.local>
2021-05-12 11:43:33 +0300

    lector about Find, Get and Eq

---
+++ cypress/integration/first.spec.js
@@ -0,0 +1,55 @@
+/// <reference types="Cypress"/>
+
+/*it ('By ID', () => {
+    cy.visit('https://facebook.com/')
+    cy.get('#email')
+})
+
+
+it ('By Class', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+    cy.get('.DocSearch-Button-Placeholder')
+})
+
+
+it ('By Tag', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+    cy.get('nav')
+})
+
+it ('By Tag value', () => {
+    cy.visit('https://facebook.com/')
+    cy.get('[name="pass"]')
+})
+
+
+it ('By Different Tag value', () => {
+    cy.visit('https://facebook.com/')
+    cy.get('[data-testid="open-registration-form-button"][role="button"]')
+})
+
+
+it('By Different Types', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+    cy.get('button[type="button"][title="close banner"]')
+})
+
+
+
+it.only ('By Contains Name', () => {
+    cy.visit('https://next.privat24.ua/')
+    cy.get('*[class^="card"]')
+})
+*/
+
+
+it ('Using Get with Find and Eq', () => {
+    cy.visit('https://next.privat24.ua/deposit/open')
+    cy.get('tbody').find('td').find('div').find('button').eq(0)
+})
+
+it.only ('Using Get with Find and Eq', () => {
+    cy.viewport(1800, 700)
+    cy.visit('https://docs.cypress.io/api/commands/eq.html#Syntax')
+    cy.get('aside').find('div').find('ol').find('li').find('a').eq(0)
+});
